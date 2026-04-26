ace6bfb9d0ee11182d8f5d6cfc5edff963f15533
Simon Ross <simonross@cedarville.edu>
2026-04-25 23:46:04 -0400

    add work

--- MATLAB/part2.m
+++ MATLAB/part2.m
@@ -2,32 +2,32 @@ clear
 %% Load and Define Constants
 constants
 constants_part2_3
-%P_CPU =                             %CPU Power, W, VARIABLE
-%P_fan =                             %Fan Power, W, VARIABLE
+P_CPU = 1000;                            %CPU Power, W, VARIABLE
+P_fan = 1000;                            %Fan Power, W, VARIABLE
+P_tot = P_CPU + P_fan;
+CPU_perf = 39000*(1-exp(-.02*P_CPU))     %CPU Performance Score
+Q_fan = (100000*(P_fan/10000)^(1/3))/(1000*60)   %Fan Flowrate, m^3/s
 
 %% Calculate Thermal Resistances
-R_paste = 1/(h_c * A_c_CPUs)
-R_cond = t_DUCT/(k * A_c_wall)
+R_paste = 1/(h_c * A_c_CPUs);            
+R_cond = t_DUCT/(k * A_c_wall);
 
 %% Calculate Air Density (Assume temperature, pressure??)
-T_bulk = (T_me + T_mi)/2 - 273.15 %celcius
-rho = 1.145; %assumed based on a temperature of T_bulk, FROM TABLE
-nu = 1.655 * 10e-5; %assumed based on a temperature of T_bulk, FROM TABLE
-Pr = 0.7268; %Prandtl number, assumed based on a temperature of T_bulk, FROM TABLE
-
-%% Iteratively solve for Q_dot_tot
-sim('simulink_recursive_solve');
+T_bulk = (T_me + T_mi)/2 - 273.15    %celcius
+rho = 1.145 * 1000;                  %g/m^3, based on a temperature of T_bulk, FROM TABLE
+nu = 1.655 * 10e-5;                  %m^2/s assumed based on a temperature of T_bulk, FROM TABLE
+Pr = 0.7268;                         %Prandtl number, assumed based on a temperature of T_bulk, FROM TABLE
 
 %% Calculate mass flow rate
 %given T_me = T_mi + Q_dot/(m_dot * c_p)
-m_dot_air = simOut.Q_dot_tot/(c_p*(T_me-T_mi)) %this is the necessary m_dot
+m_dot_air = Q_fan * rho;             %g/s %Q_dot_tot/(c_p*(T_me-T_mi)) %commented out is the necessary m_dot - requires iterative solution
 
 %% Calculate Mean Velocity
 %assume conservation of mass to use:
 u_m = m_dot_air/(rho*A_c)
 
 %% Calculate Surrogate Duct Reynolds Number
-Re_D = (u_m * D_h) / nu; %Reynolds number for the duct
+Re_D = (u_m * D_h) / nu;             %Reynolds number for the duct
 
 %% Calculate Convection Heat Transfer Coefficient
 %assume turbulent
@@ -52,20 +52,20 @@ A_tot = A_fins + A_unfin;            %total area of fin bank, meters^2
 %R_tot = 1 / (h * A_tot * eta_tot) 
 R_fins = 1 / (h * A_fins * eta_fin); %fins convection thermal resistance, K/W
 R_unfin = 1 / (h * A_unfin);         %unfin convection thermal resistance, K/W
-R_tot = 1/(1/R_fins+1/R_unfin);      %total resistance - parallel resistors, K/W
+R_tot = 1/(1/R_fins+1/R_unfin);      %total resistance - parallel fin and unfin resistors, K/W
 
 %% Find Surface Temperatures
-%from resistor eqn:
-T_b = Q_dot_tot*(R_paste+R_cond) + 90; %celcius
+%from resistor eqn, equating current:
 T_inf = T_bulk; %celcius IS THIS A GOOD ASSUMPTION???
+T_b = (90*R_tot+T_inf*(R_paste+R_cond))/(R_tot+R_paste+R_cond) %Q_dot_tot*(R_paste+R_cond) + 90; %celcius, commented out is old work - equated around Q_dot_tot
 
 %% a) Determine Maximum allowable power dissipation per transistor
-Q_dot_tot = (T_b-T_inf)/R_tot;       %heat transfer rate, W
-Q_dot_tran = Q_dot_tot/N_CPU         %allowed power per transistor
+Q_dot_tot = (T_b-T_inf)/R_tot       %heat transfer rate, W
+Q_dot_tran = Q_dot_tot/N_CPU         %allowed power per transistor, W
 
 %% b) Determine Required Volume Flow Rate of Air
 %assume perfectly caloric
-m_dot_air = Q_dot_tot/(c_p*dT_air)   %minimum volume flow rate, kg/s
+%m_dot_air = Q_dot_tot/(c_p*dT_air)   %minimum volume flow rate, kg/s
 
 %% Simulate
 
